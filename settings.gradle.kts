64ef13c8419c0022f73c9ffa38ec294a1dd94bcc
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-25 18:57:21 +0530

    Add mavenContent filters to Google repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,13 @@ plugins {
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
