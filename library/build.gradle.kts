8867172029dadffe9fe5d10f9bc70a16dad4034e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-27 08:33:04 +0530

    Add the JVM target to the KMP setup of the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
 }
 
 layout.buildDirectory.set(file("$rootDir/.build/library"))
