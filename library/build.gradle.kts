83f977f46cfd479d838658cbd4d78be64c81d391
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-27 08:36:17 +0530

    Define the library module's configuration and publishing information from the version catalog

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
 
+group = libs.versions.library.group.get()
+version = libs.versions.library.version.get()
+
 kotlin {
     jvm("desktop") {
         compilerOptions {
