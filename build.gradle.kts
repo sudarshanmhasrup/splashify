eabdeeef5dab9b71c1c6167cf24c90e512193b13
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-27 08:43:21 +0530

    Add the "com.vanniktech.maven.publish" plugin to the root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
 
 layout.buildDirectory.set(file("$rootDir/.build/project"))
