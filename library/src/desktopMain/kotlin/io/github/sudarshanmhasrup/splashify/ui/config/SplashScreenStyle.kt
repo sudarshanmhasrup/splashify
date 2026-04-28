43ab205537007d1d21e2a0ae454b3668f134fd1c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 08:59:40 +0530

    Add documentation to SplashScreenStyle

--- library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/ui/config/SplashScreenStyle.kt
+++ library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/ui/config/SplashScreenStyle.kt
@@ -4,6 +4,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+/**
+ * Defines the visual style of the splash screen.
+ *
+ * @param backgroundColor Background color of the splash screen.
+ * @param cornerRadius Corner radius of the splash screen.
+ */
 data class SplashScreenStyle(
     val backgroundColor: Color = Color.White,
     val cornerRadius: Dp = 8.dp
