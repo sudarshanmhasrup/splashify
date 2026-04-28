a4aef9c9157ff3b25530d416dac67d3811ec4505
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 20:03:54 +0530

    Add documentation for SplashifyApp

--- library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/SplashifyApp.kt
+++ library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/SplashifyApp.kt
@@ -6,6 +6,16 @@ import androidx.compose.runtime.remember
 import io.github.sudarshanmhasrup.splashify.internal.LocalSplashScreenStateManager
 import io.github.sudarshanmhasrup.splashify.internal.SplashScreenStateManager
 
+/**
+ * Entry point for Splashify.
+ *
+ * Displays the splash screen first and switches to the main content when the splash screen is finished.
+ *
+ * Internally manages splash state and provides it to splash composables.
+ *
+ * @param splashScreen Composable shown as the splash screen.
+ * @param content Composable shown after the splash screen completes.
+ */
 @Composable
 fun SplashifyApp(
     splashScreen: @Composable () -> Unit,
