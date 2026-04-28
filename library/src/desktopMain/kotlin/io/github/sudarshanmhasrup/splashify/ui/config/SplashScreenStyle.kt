14abb8cf0a6b8bc1b29195923b8fe2fe790d1f8d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 08:24:27 +0530

    Add SplashScreenStyle for UI configuration

---
+++ library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/ui/config/SplashScreenStyle.kt
@@ -0,0 +1,10 @@
+package io.github.sudarshanmhasrup.splashify.ui.config
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class SplashScreenStyle(
+    val backgroundColor: Color = Color.White,
+    val cornerRadius: Dp = 20.dp
+)
