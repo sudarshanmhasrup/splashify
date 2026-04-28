541729d62108dd2f6ecc4f5cc659df2d4895cf89
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 20:02:15 +0530

    Implement splash screen progress and visibility logic

--- library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/internal/SplashScreenStateManager.kt
+++ library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/internal/SplashScreenStateManager.kt
@@ -5,13 +5,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 internal class SplashScreenStateManager {
     var showSplashScreen by mutableStateOf(true)
     var loaderProgress by mutableStateOf(0f)
 
+    private val scope = CoroutineScope(Dispatchers.Main)
+
     init {
-        
+        scope.launch {
+            for (i in 0..10) {
+                loaderProgress++
+                delay(timeMillis = 250)
+            }
+
+            showSplashScreen = false
+        }
     }
 }
 
