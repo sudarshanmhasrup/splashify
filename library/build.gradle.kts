dc56147ff62222aacc1f9cb0cb1d6850a8351bd2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 20:13:00 +0530

    Add the "org.jetbrains.kotlinx:kotlinx-coroutines-core" dependency the JVM source set of the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin {
     sourceSets {
         val desktopMain by getting
         desktopMain.dependencies {
+            implementation(libs.kotlinx.coroutines)
             implementation(libs.bundles.compose.multiplatform)
         }
     }
