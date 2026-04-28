3019b177e761ad6d588ed10193a0608fc3eaaf15
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 08:19:26 +0530

    Add SplashScreenSize data class for desktop configuration

---
+++ library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/ui/config/SplashScreenSize.kt
@@ -0,0 +1,9 @@
+package io.github.sudarshanmhasrup.splashify.ui.config
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class SplashScreenSize(
+    val width: Dp = 850.dp,
+    val height: Dp = 550.dp
+)
