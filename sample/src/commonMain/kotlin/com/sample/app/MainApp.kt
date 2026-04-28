cdc475971841febfcf652312588f36e44c17a182
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-28 20:25:43 +0530

    Integrate SplashifyApp into the application entry point

--- sample/src/commonMain/kotlin/com/sample/app/MainApp.kt
+++ sample/src/commonMain/kotlin/com/sample/app/MainApp.kt
@@ -1,9 +1,14 @@
 package com.sample.app
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.application
+import io.github.sudarshanmhasrup.splashify.SplashifyApp
 
 fun main() {
     application {
-        
+        SplashifyApp(splashScreen = { SplashScreen() }) {
+            MainWindow(modifier = Modifier.fillMaxSize())
+        }
     }
 }
