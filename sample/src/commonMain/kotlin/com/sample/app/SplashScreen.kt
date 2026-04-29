3d9fd48d40761013d0eab5f40e8d0acb06abc8eb
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-29 08:59:10 +0530

    Update SplashScreen to use ProgressiveSplashScreen and add indicator styling

--- sample/src/commonMain/kotlin/com/sample/app/SplashScreen.kt
+++ sample/src/commonMain/kotlin/com/sample/app/SplashScreen.kt
@@ -6,16 +6,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.github.sudarshanmhasrup.splashify.ui.config.SplashIndicatorStyle
 import io.github.sudarshanmhasrup.splashify.ui.config.SplashScreenSize
 import io.github.sudarshanmhasrup.splashify.ui.config.SplashScreenStyle
-import io.github.sudarshanmhasrup.splashify.ui.splashscreen.SimpleSplashScreen
+import io.github.sudarshanmhasrup.splashify.ui.splashscreen.ProgressiveSplashScreen
 
 @Composable
 fun SplashScreen() {
     val size = SplashScreenSize(width = 600.dp, height = 380.dp)
     val style = SplashScreenStyle(cornerRadius = 16.dp, backgroundColor = Color.Black)
+    val indicatorStyle = SplashIndicatorStyle(color = Color.White, trackColor = Color.Black, thickness = 2.dp)
 
-    SimpleSplashScreen(size = size, style = style) {
+    ProgressiveSplashScreen(size = size, style = style, indicatorStyle = indicatorStyle) {
         Text(text = "Splashify", fontSize = 28.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
     }
 }
