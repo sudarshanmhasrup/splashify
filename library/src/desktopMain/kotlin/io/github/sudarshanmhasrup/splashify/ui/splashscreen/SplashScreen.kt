695bf25d2140d61f72618879d30d1ec3b79538d7
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-04-29 08:58:39 +0530

    Remove unused imports from the SplashScreen.kt

--- library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/ui/splashscreen/SplashScreen.kt
+++ library/src/desktopMain/kotlin/io/github/sudarshanmhasrup/splashify/ui/splashscreen/SplashScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
